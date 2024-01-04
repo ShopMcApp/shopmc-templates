e537e71ff4d9f3332391c15ad40677776950acc3
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-01-04 11:43:21 +0100

    use session as key

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const fetch = require('node-fetch');
 
 app.set('view engine', 'ejs');
 
-const customer = process.env.CUSTOMER
 const baseURL = process.env.BASE_URL
+const session = process.env.SESSION
 const template = process.env.TEMPLATE
 
 app.get('/', async function (req, res) {
-    const data = await fetch(`${baseURL}/api/shop/${customer}`).then(res => res.json())
-    res.render(template, { shop: data });
+    const data = await fetch(`${baseURL}/api/user/shop`,{
+        headers: {
+            'Cookie': `session=${session}`
+        }
+    }).then(res => res.json())
+    console.log(data)
+    // res.render(template, { shop: data });
 });
 
 app.listen(3000);
