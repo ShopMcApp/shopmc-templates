4b0f35f2f42911db793e8b7db19dbe9e6370c43f
Michał Jażdżyk <michal.jazdzyk.007@gmail.com>
2024-01-06 23:19:39 +0100

    small fixes

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-require('dotenv').config()
+const fetch = require('node-fetch');
 const express = require('express');
 const app = express();
-const fetch = require('node-fetch');
 
 app.set('view engine', 'ejs');
+
+require('dotenv').config()
 
 const baseURL = process.env.BASE_URL
 const session = process.env.SESSION
 const template = process.env.TEMPLATE
 
 app.get('/', async function (req, res) {
-    const data = await fetch(`${baseURL}/api/user/shop`,{
-        headers: {
-            'Cookie': `session=${session}`
-        }
+    const shop = await fetch(`${baseURL}/api/user/shop`, {
+        headers: { 'Cookie': `session=${session}` }
     }).then(res => res.json())
-    res.render(template, { shop: data });
+    res.render(template, { shop });
 });
 
 app.listen(3000);
